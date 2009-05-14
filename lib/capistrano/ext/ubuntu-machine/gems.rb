bb2ce4595110420683a870ff8a3d50f5934c9ef6
Nate Zobrist <zobie@zobie.com>
2009-05-14 01:10:39 -0600

    When runing install_rubygems, overwrite the /usr/bin/gem symlink if it already exists.

--- lib/capistrano/ext/ubuntu-machine/gems.rb
+++ lib/capistrano/ext/ubuntu-machine/gems.rb
@@ -1,15 +1,16 @@
 namespace :gems do
-  desc "Install RubyGems"
+
+  desc "Install RubyGems from source."
   task :install_rubygems, :roles => :app do
     run "wget http://rubyforge.org/frs/download.php/45905/rubygems-#{rubygem_version}.tgz"
     run "tar xvzf rubygems-#{rubygem_version}.tgz"
     run "cd rubygems-#{rubygem_version} && sudo ruby setup.rb"
-    sudo "ln -s /usr/bin/gem1.8 /usr/bin/gem"
+    sudo "ln -fs /usr/bin/gem1.8 /usr/bin/gem"
     sudo "gem update"
     sudo "gem update --system"
     run "rm -Rf rubygems-#{rubygem_version}*"
   end
-  
+
   desc "List gems on remote server"
   task :list, :roles => :app do
     stream "gem list"
@@ -19,7 +20,7 @@ namespace :gems do
   task :update, :roles => :app do
     sudo "gem update"
   end
-  
+
   desc "Update gem system on remote server"
   task :update_system, :roles => :app do
     sudo "gem update --system"
@@ -36,4 +37,5 @@ namespace :gems do
     name = Capistrano::CLI.ui.ask("Which gem should we uninstall: ")
     sudo "gem uninstall #{name}"
   end
+
 end
