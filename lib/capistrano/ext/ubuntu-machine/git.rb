29b581e3f7d86f06e88730e46836290cf54951a7
Nate Zobrist <zobie@zobie.com>
2009-05-25 00:24:22 -0600

    Modify git.rb to automatically install the latest released version. No more dependency on :git_version in deploy.rb.

--- lib/capistrano/ext/ubuntu-machine/git.rb
+++ lib/capistrano/ext/ubuntu-machine/git.rb
@@ -1,15 +1,50 @@
+require 'net/http'
+
 namespace :git do
-  desc "Install git"
+
+  desc "Install the latest version of git from source."
   task :install, :roles => :app do
-    sudo "sudo apt-get build-dep git-core -y"
-    run "wget http://kernel.org/pub/software/scm/git/#{git_version}.tar.gz"
-    run "tar xvzf #{git_version}.tar.gz"
-    run "cd #{git_version}"
-    run "cd #{git_version} && ./configure"
-    run "cd #{git_version} && make"
-    run "cd #{git_version} && sudo make install"
-    run "rm #{git_version}.tar.gz"
-    run "rm -Rf #{git_version}"
+    latest = latest_version
+    if (current_version != latest)
+      sudo "apt-get build-dep git-core -y"
+      run "wget -q #{git_url}"
+      run "tar xvzf git-#{latest}.tar.gz"
+      run "cd git-#{latest} && auto-apt run ./configure && make"
+      run "cd git-#{latest} && #{sudo} checkinstall -y"
+      sudo "rm -rf git-#{latest}"
+      run "rm git-#{latest}.tar.gz"
+    end
+  end
+
+  desc "Remove git from the server."
+  task :uninstall do
+    sudo "dpkg -r git"
+  end
+
+  desc "Alias for git:install."
+  task :update do
+    install
+  end
+
+  # Gets the version of git which is currently installed on the server.
+  set :current_version do
+    begin
+      out = capture "git --version"
+      out.split(/((\d\.?)+$)/)[1]
+    rescue # if git isn't installed
+      "0.0.0"
+    end
+  end
+
+  # Gets the most recently released version of git.
+  set :latest_version do
+    git_url.split(/git-((\d\.?)+).tar\.gz/)[1]
+  end
+
+  # Gets the url to download the most recently released version of git.
+  set :git_url do
+    latest = Net::HTTP.get('kernel.org', '/pub/software/scm/git/').scan(/(git-(\d\.?)+.tar\.gz)/).last[0]
+    "http://kernel.org/pub/software/scm/git/#{latest}"
   end
 
 end
